f078f7295b8133f521af7306a4aafd36680fa347
Christos Cunning <christoscunn@gmail.com>
2018-02-05 13:55:45 -0500

    Shitty but working controls (no drift)

--- src/entities/Ship.java
+++ src/entities/Ship.java
@@ -1,9 +1,12 @@
 package entities;
 
 import processing.core.PShape;
+import main.Main;
 import processing.core.PApplet;
 
 /** The player controlled character: ship class
+ * 
+ *  TODO: Fix controls
  * 
  * @author Christos Cunning
  *
@@ -11,12 +14,19 @@ import processing.core.PApplet;
 
 public class Ship {
 	
+	public static final float MAX_SPEED = 5;
+	
 	private float x,y; //position of ship
 	private float dir; //direction of ship in radians
 	private float spd; //speed of ship
-	private float size;
+	private float size; // pseudo radius
 	
 	PShape ship;
+	
+	public boolean isAccelerating;
+	public boolean isDecelerating;
+	public boolean isTurningCW;
+	public boolean isTurningCCW;
 	
 	/** Constructor method for ship object
 	 *  Player controlled character
@@ -33,6 +43,11 @@ public class Ship {
 		this.dir = (float) (dir + (3*Math.PI)/2);
 		this.spd = spd;
 		this.size = size;
+		
+		isAccelerating = false;
+		isDecelerating = false;
+		isTurningCW = false;
+		isTurningCCW = false;
 	}
 	
 	/** Move method for ship class
@@ -50,11 +65,41 @@ public class Ship {
 		y += spd * PApplet.sin(dir);
 		
 		//Next check for edge of screen and wrap around
+		if (x+size > Main.SCREEN_W) {
+			x = 0+size;
+		}
+		if (x-size < 0) {
+			x = Main.SCREEN_W-size;
+		}
+		if (y+size > Main.SCREEN_H) {
+			y = 0 + size;
+		}
+		if (y-size < 0) {
+			y = Main.SCREEN_H-size;
+		}
 	}
 	
 	/* */
 	public void draw(PApplet p) {
 		p.shape(ship,x,y);
+	}
+	
+	public void speedUp(float ammount) {
+		spd += ammount;
+	}
+	
+	public void speedDown(float ammount) {
+		spd -= ammount;
+	}
+	
+	/** Turns the player controlled ship sprite by a specified angle, given in degrees.
+	 *  positive angles turn the ship clockwise and vice versa.
+	 * 
+	 * @param angle the angle to rotate by, given in degrees
+	 */
+	public void turn (float angle) {
+		dir += Math.toRadians((double) angle);
+		ship.rotate((float) Math.toRadians((double) angle));
 	}
 	
 	/* Run once at launch of application to create custom shape for ship*/
@@ -71,6 +116,11 @@ public class Ship {
 		ship.vertex(0, -size*2);
 		
 		ship.endShape();
+	}
+	
+	/* Getters and setters */
+	public float getSpeed() {
+		return spd;
 	}
 	
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -5,8 +5,8 @@ import entities.*;
 
 public class Main extends PApplet {
 	
-	private final int SCREEN_W = 800;
-	private final int SCREEN_H = 600;
+	public static final int SCREEN_W = 800;
+	public static final int SCREEN_H = 600;
 	
 	Ship player = new Ship(SCREEN_W/2,SCREEN_H/2,0,0,10);
 	
@@ -14,14 +14,64 @@ public class Main extends PApplet {
 	public void draw() {
 		clear();
 		
+		// Run keyPressed function to check for user input
+		
+		
+		
 		
 		// Move then draw the player sprite
+		if(player.isAccelerating) {
+			if(player.getSpeed() < Ship.MAX_SPEED) player.speedUp(1);
+		} else if (player.isDecelerating) {
+			if(player.getSpeed() > -Ship.MAX_SPEED) player.speedDown(1);
+		} else {
+			if(player.getSpeed() > 0) {
+				player.speedDown(0.05f);
+			} else if(player.getSpeed() < 0) {
+				player.speedUp(0.05f);
+			}
+		}
+		if(player.isTurningCW) {
+			player.turn(5);
+		} else if (player.isTurningCCW) {
+			player.turn(-5);
+		}
 		player.move();
 		player.draw(this);
 		
 		
 	}
 	
+	/* Checking for keyboard input from user */
+	public void keyPressed() {
+		if (key == 'w' || key == 'W') {
+			player.isAccelerating = true;
+		}
+		if (key == 's' || key == 'S') {
+			player.isDecelerating = true;
+		}
+		if (key == 'd' || key == 'D') {
+			player.isTurningCW = true;
+		}
+		if (key == 'a' || key == 'A') {
+			player.isTurningCCW = true;
+		}
+	}
+	
+	public void keyReleased() {
+		if (key == 'w' || key == 'W') {
+			player.isAccelerating = false;
+		}
+		if (key == 's' || key == 'S') {
+			player.isDecelerating = false;
+		}
+		if (key == 'd' || key == 'D') {
+			player.isTurningCW = false;
+		}
+		if (key == 'a' || key == 'A') {
+			player.isTurningCCW = false;
+		}
+	}
 	
 	
 	/* Run once after drawing stuff is available */
